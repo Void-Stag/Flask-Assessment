879100f06a2f5ab8e3118aee53de8696de5734c7
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2022-10-31 20:35:29 +0000

    Revert "event page route"
    This reverts commit a155a01dc946dbcd26a52c3ddd5f546b9c77e8ec.

--- App3_Scrum_080222/app/main/routes.py
+++ App3_Scrum_080222/app/main/routes.py
@@ -7,14 +7,3 @@ main = Blueprint('main', __name__)
 @main.route('/index', methods=['GET', 'POST'])
 def index(cat=None):
     return render_template('index.html',  title='Home')
-<<<<<<< Updated upstream
-=======
-
-@main.route('/contact', methods=['GET','POST'])
-def contact(cat=None):
-    return render_template('contact.html', title='Contact')
-
-@main.route('/events', methods=['GET', 'POST'])
-def events(cat=None):
-    return render_template('events.html', title='Events')
->>>>>>> Stashed changes
