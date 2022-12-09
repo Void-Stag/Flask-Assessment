90e8312d05fdc65196c9df49c21592f95c938ef9
Heather Smith <64465757+Void-Stag@users.noreply.github.com>
2022-12-09 17:00:35 +0000

    Update routes.py

--- App3_Scrum_080222/app/main/routes.py
+++ App3_Scrum_080222/app/main/routes.py
@@ -9,10 +9,6 @@ main = Blueprint('main', __name__)
 def index(cat=None):
     return render_template('index.html',  title='Home')
 
-@main.route('/contact', methods=['GET','POST'])
-def contact(cat=None):
-    return render_template('contact.html', title='Contact')
-
 @main.route('/events', methods=['GET','POST'])
 def events(cat=None):
     return render_template('event.html', title='Events')
